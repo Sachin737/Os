cc7aa13009226d33a92985401d76326aed1affb8
sachin <b20129@students.iitmandi.ac.in>
2024-02-06 23:14:26 +0530

    segmentation

--- virtualization/Segmentation.c
+++ virtualization/Segmentation.c
@@ -3,8 +3,23 @@
 #include<stdlib.h>
 #include<string.h>
 
+/*
+    *   Since only base+offset mechanism has some issues, we have better approach called segmentation.
+    *   In segmentation, instead of mapping whole process data to single chunk in physical memory, we segment code,
+        stack and heap and map them to different physical locations (each with its own base and limit values).
+    *   To further improve memory usage, we add extra protection bit which indicates if that particular segment
+        has read,write or both options! If read only segmenet present than we can share this segment with multiple
+        processes (if needed).
+    *   
+
+    *   Source: https://pages.cs.wisc.edu/~remzi/OSTEP/vm-segmentation.pdf
+                https://student.cs.uwaterloo.ca/~cs350/W19/notes/virtualmemory-1up.pdf [from page 11]
+
+
+*/
+
 int main(int argc, char* argv[]){
-    
+
 
     return 0;
 }
